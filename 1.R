8f84514a7131cad2df0b78e368c2a358621e6874
unknown <tw19620202@gmail.com>
2019-04-15 11:00:53 +0800

    2

---
+++ 1.R
@@ -0,0 +1,10 @@
+rm(list=ls())
+
+ifelse(!require(quantmod),install.packages('quantmod'),library(quantmod))
+tw20_adj_close<-read.csv("2018Q4_20.csv")
+
+ifelse(!require(readr),install.packages('readr'),library(readr))
+tw20.txt<-read.table("tw20.txt")
+
+ifelse(!require(reshape2),install.packages('reshape2'),library(reshape2))
+tw20.xst<-dcast(etf4.c,date~id)
